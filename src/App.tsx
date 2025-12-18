9b236d76afc0bfce161ce585e0b39af45280ba4e
saifxss <chamakhiseif@gmail.com>
2025-12-18 23:59:46 +0100

    chore: setup React + TypeScript project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <p>Welcome</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header>
+      <h2>Notion Lite</h2>
+    </header>
+  );
+}
+
+export default Header;
